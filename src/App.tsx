fa53dedb4c444ac402c206c38efa801c9a6c60db
maksimselchukou <selchukouwebdev1995@gmail.com>
2022-05-12 09:32:55 +0300

    Add Array Tasks,add type Array Tasks,use hook UseState

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+export type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [tasks, setTasks] = useState<Array<TaskType>>([
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "ReactJS", isDone: false}
+    ])
+
+
+    return (
+        <div className="App">
+            Create Todolist!
+        </div>
+    );
 }
 
 export default App;
